34a740d8130adcb589da327bb932a07b21381621
Antonio Ochoa <antonhub00@gmail.com>
2019-02-15 11:58:23 -0600

    Notes and excercise in DBA modified

---
+++ DBA/unit_1/exercises/excercise_1.sql
@@ -0,0 +1,11 @@
+--Crecimiento anual
+50000 / dia = 128,250,000
+
+--Promedio diario 50 mil transacciones
+21 bytes x 50 transacciones.
+
+--Porcentaje de uso de disco duro  DD = 500 Gb
+500 = 100%
+0.35 = x
+
+x = 0.07%
