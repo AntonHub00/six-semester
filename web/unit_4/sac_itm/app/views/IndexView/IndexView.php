62dac4d373be5aa2eea9df1cb8c0182ff30019c6
Antonio Ochoa <antonhub00@gmail.com>
2019-05-18 17:45:57 -0500

    Show student data and register student implemented. htaccess "rules" added
    Changed DB class so set DB charset before return connection. This is helpful with
    languages like spanish because allows you to display correctly tildes and accents.
    
    htaccess file was modified so "static" folder is available for everybody. This also
    allows you to link JS and CSS files so it doesn't get redirected by rule set before.
    
    Missing:
    Implement sessions.

--- web/unit_4/sac_itm/app/views/IndexView/IndexView.php
+++ web/unit_4/sac_itm/app/views/IndexView/IndexView.php
@@ -12,99 +12,118 @@
   <head>
     <meta charset="UTF-8">
     <title>Login</title>
-    <link rel="stylesheet" type="text/css" href="<?php echo(SITE_URL);?>/static/css/login.css">
+    <link rel="stylesheet" type="text/css" href="<?php echo SITE_URL;?>/static/css/login.css">
   </head>
   <body>
     <div class="wrapper">
       <header class="main-header">
-	finanzapp
+	sac-itm
       </header>
-      <form action="<?php echo(SITE_URL);?>/index.php" method="POST" class="signin-wrapper">
+      <form action="<?php echo SITE_URL;?>\" method="POST" class="signin-wrapper">
 	<div class="title-box">
           Registrarse
 	</div>
-	<div class="signin-grid-item username-box">
-          <label for="username">Usuario</label>
-          <input type="text" id="signin-username" name="username"
-		 placeholder="Usuario" maxlength="20" class="" required>
+	<div class="signin-grid-item control-num-box">
+          <label for="id">Número de control</label>
+	  <input name="id" type="text" required/>
 	</div>
 	<div class="signin-grid-item name-box">
           <label for="name">Nombre</label>
-          <input type="text" id="signin-name" name="name" placeholder="Nombre"
-		 maxlength="50" class="" required>
-	</div>
-	<div class="signin-grid-item slastname-box">
-          <label for="slastname">Apellido Paterno</label>
-          <input type="text" id="signin-flastname" name="flastname"
-		 placeholder="Apellido Paterno" maxlength="50" class="" required>
+	  <input name="nombre" type="text" maxlength="35" required/>
 	</div>
 	<div class="signin-grid-item flastname-box">
-          <label for="flastname">Apellido Materno</label>
-          <input type="text" id="signin-slastname" name="slastname"
-		 placeholder="Apellido Materno" maxlength="50" class="" required>
+          <label for="slastname">Primer apellido</label>
+	  <input name="primer_apellido" type="text" maxlength="35" required/>
 	</div>
-	<div class="signin-grid-item birthdate-box">
-          <label for="birthdate">Fecha de nacimiento</label>
-          <input type="date" id="signin-birthdate" name="birthdate"
-		 maxlength="15" class="" required>
+	<div class="signin-grid-item slastname-box">
+          <label for="flastname">Segundo apellido</label>
+	  <input name="segundo_apellido" type="text" maxlength="35" required/>
+	</div>
+	<div class="signin-grid-item career-box">
+          <label for="id_carrera">Carrera</label>
+          <select name="id_carrera" >
+	    <?php foreach ($vars['careers'] as $career): ?>
+	      <?php if ($career['id'] == $student_data['id_carrera']): ?>
+		<option value="<?php echo $career['id']; ?>" selected="selected">
+		  <?php echo $career['descripcion']; ?>
+		</option>
+	      <?php else: ?>
+		<option value="<?php echo $career['id']; ?>">
+		  <?php echo $career['descripcion']; ?>
+		</option>
+	      <?php endif; ?>
+	    <?php endforeach; ?>
+	  </select>
+	</div>
+	<div class="signin-grid-item semester-box">
+	  <label for="id_semestre">Semestre</label>
+	  <select name="id_semestre">
+	    <?php foreach ($vars['semesters'] as $semester): ?>
+	      <?php if ($semester['id'] == $student_data['id_semestre']): ?>
+		<option value="<?php echo $semester['id']; ?>" selected="selected">
+		  <?php echo $semester['id']; ?>
+		</option>
+	      <?php else: ?>
+		<option value="<?php echo $semester['id']; ?>">
+		  <?php echo $semester['id']; ?>
+		</option>
+	      <?php endif; ?>
+	    <?php endforeach; ?>
+	  </select>
 	</div>
 	<div class="signin-grid-item email-box">
-          <label for="email">Correo</label>
-          <input type="email" pattern="\b.{1,50}\b" title="Máximo 50 caracteres"
-		 id="signin-email" name="email" placeholder="Correo" class="" required>
+          <label for="password">Correo</label>
+	  <input name="correo" type="text" maxlength="35" required/>
 	</div>
 	<div class="signin-grid-item phone-box">
-          <label for="phone">Número de teléfono</label>
-          <input type="text" pattern="\d{10}" title="Deben ser 10 dígitos"
-		 id="signin-phone" name="phone" placeholder="Número de teléfono"
-		 class="" required>
-	</div>
-	<div class="signin-grid-item password-box">
-          <label for="password">Contraseña</label>
-          <input type="password" id="signin-password" name="password"
-		 placeholder="Contraseña" maxlength="15" class="" required>
-	</div>
-	<div class="signin-grid-item otro1">
-          <label for="password">Contraseña</label>
-          <input type="password" id="signin-password" name="password"
-		 placeholder="Contraseña" maxlength="15" class="" required>
-	</div>
-	<div class="signin-grid-item otro2">
-          <label for="password">Contraseña</label>
-          <input type="password" id="signin-password" name="password"
-		 placeholder="Contraseña" maxlength="15" class="" required>
-	</div>
-	<div class="signin-grid-item otro3">
-          <label for="password">Contraseña</label>
-          <input type="password" id="signin-password" name="password"
-		 placeholder="Contraseña" maxlength="15" class="" required>
-	</div>
-	<div class="signin-grid-item otro4">
-          <label for="password">Contraseña</label>
-          <input type="password" id="signin-password" name="password"
-		 placeholder="Contraseña" maxlength="15" class="" required>
-	</div>
-	<div class="signin-grid-item otro5">
-          <label for="password">Contraseña</label>
-          <input type="password" id="signin-password" name="password"
-		 placeholder="Contraseña" maxlength="15" class="" required>
-	</div>
-	<div class="signin-grid-item otro6">
-          <label for="password">Contraseña</label>
-          <input type="password" id="signin-password" name="password"
-		 placeholder="Contraseña" maxlength="15" class="" required>
-	</div>
-	<div class="signin-grid-item otro7">
-          <label for="password">Contraseña</label>
-          <input type="password" id="signin-password" name="password"
-		 placeholder="Contraseña" maxlength="15" class="" required>
-	</div>
-	<div class="signin-grid-item submit-box">
-          <button type="submit" name="posted" value="signin">Registrarse</button>
-	</div>
+          <label for="phone">Teléfono</label>
+	  <input name="telefono" type="text" maxlength="15" required/>	</div>
+	  <div class="signin-grid-item gender-box">
+            <label for="password">Género</label>
+	    <select name="id_genero" >
+	      <?php foreach ($vars['genders'] as $gender): ?>
+		<?php if ($gender['id'] == $student_data['id_genero']): ?>
+		  <option value="<?php echo $gender['id']; ?>" selected="selected">
+		    <?php echo $gender['descripcion']; ?>
+		  </option>
+		<?php else: ?>
+		  <option value="<?php echo $gender['id']; ?>">
+		    <?php echo $gender['descripcion']; ?>
+		  </option>
+		<?php endif; ?>
+	      <?php endforeach; ?>
+	    </select>
+	  </div>
+	  <div class="signin-grid-item password-box">
+            <label for="password">Contraseña</label>
+	    <input name="contrasena" type="password" maxlength="80" required/>
+	  </div>
+	  <div class="signin-grid-item tutor-name-box">
+            <label for="password">Nombre tutor</label>
+	    <input name="nombre_tutor" type="text" maxlength="35" required/>
+	  </div>
+	  <div class="signin-grid-item tutor-flastname-box">
+            <label for="password">Primer apellido tutor</label>
+	    <input name="primer_apellido_tutor" type="text" maxlength="35" required/>
+	  </div>
+	  <div class="signin-grid-item tutor-slastname-box">
+            <label for="password">Segundo apellido tutor</label>
+	    <input name="segundo_apellido_tutor" type="text" maxlength="35" required/>
+	  </div>
+	  <div class="signin-grid-item tutor-phone-box">
+            <label for="password">Teléfono tutor</label>
+	    <input name="telefono_tutor" type="text" maxlength="15" required/>
+	  </div>
+	  <div class="signin-grid-item tutor-email-box">
+            <label for="password">Correo tutor</label>
+	    <input name="correo_tutor" type="email" maxlength="35" required/>
+	  </div>
+	  <div class="signin-grid-item submit-box">
+            <button type="submit" name="submit" value="signin">Registrarse</button>
+	  </div>
       </form>
 
-      <form action="<?php echo(SITE_URL);?>/index.php" method="POST" class="login-wrapper">
+      <form action="<?php echo SITE_URL; ?>/" method="POST" class="login-wrapper">
 	<div class="title-box">
           Ingresar
 	</div>
@@ -119,7 +138,7 @@
 		 placeholder="Contraseña" maxlength="15" class="" required>
 	</div>
 	<div class="login-grid-item submit-box">
-          <button type="submit" name="posted" value="login">Ingresar</button>
+          <button type="submit" name="submit" value="login">Ingresar</button>
 	</div>
       </form>
     </div>
